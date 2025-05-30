fad5712cc0f6dd81bf357c863355a12a90bc5bad
Matthew Bailey <Matthewbailey916@gmail.com>
2025-05-30 16:14:11 -0500

    intitial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// TA assisted 
+  // const BASE_URL = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
+  // const COHORT = "/Matt-2505-FTB-ET-WEB-FT";
+  // const RESOURCE = "/events";
+  // const API = BASE_URL + COHORT + RESOURCE;
+
+  // async functions getParties (){
+  // const response = await fetch(API);
+  // console.log(response);
+  // } 
+
+
+  //  function render() {
+  //     const $app = document.querySelector(`#app`);
+  //     $app.innerHTML -`<h1> PartyPlanner </h1>`;
+  //   }
+  //   render();
+
+  //   async function init() {
+  //     await getParties()
+
+  //   }
+
+      
+
+
+// Requirements
+// For this workshop, you'll be starting from scratch! You are highly encouraged to reference previous solutions. 
+// Write JS to build an application that meets the following requirements:
+    // index.html is not modified. All elements are generated via JS.
+
+
+    // When a party name is clicked on, the application updates state by fetching information about a single party from the API.
+    // The application renders the name, ID, date, description, and location of the selected party.
+    // The application renders a message telling users to select a party if none is selected.
+    // Functions are used to organize logic involving state changes.
+    // The application is rerendered whenever state changes.
+    // UI elements are organized into component functions.
+    // All thrown errors are explicitly caught with a try...catch statement.
+    // Extensions
+    // If you're done early, try to implement one or more of the following features:
+    
+    // The name of a selected party in the party list is styled differently.
+    // The user can also see the guests who have RSVP'd to the selected party. This will involve fetching data from the /rsvps and /guests endpoints! Fetch all the guests first, and then use the RSVPs to filter the guests for the selected party.
+        
+    // The application updates state by fetching an array of parties from the API.
+        
+    // The application renders a list of party names.
+    
+    
